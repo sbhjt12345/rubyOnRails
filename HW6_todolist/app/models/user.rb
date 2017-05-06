7a9020aa864f5caa293f75002063b1ad6e20818d
Jiantao Hu <jh2468@cornell.edu>
2017-05-07 03:28:53 +0900

    Finish tmr

---
+++ HW6_todolist/app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+	has_one :profile,dependent: :destroy
+	has_many :todo_lists,dependent: :destroy
+
+	validates :username, presence: true
+
+	has_many :todo_items, through: :todo_lists, source: :todo_items,dependent: :destroy
+
+	has_secure_password
+
+	def get_completed_count
+		return todo_items.where(completed: true).count
+	end
+end
